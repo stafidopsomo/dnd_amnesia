e91cc830989dcca4031756ac849288fd9447039f
spyramida <spyros-papp@hotmail.com>
2024-12-20 13:56:23 +0000

    gibbe

--- create_character.php
+++ create_character.php
@@ -1,11 +1,22 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
 
-// Define class and subclass options
+function read_json($file) {
+    $path = __DIR__ . "/$file";
+    if (file_exists($path)) {
+        return json_decode(file_get_contents($path), true);
+    }
+    return [];
+}
+
+$barbarian_data = read_json("barbarian.json");
+$paladin_data = read_json("paladin.json");
+$rogue_data = read_json("rogue.json");
+
 $classes = [
-    "Barbarian" => ["Berserker", "Zealot"],
-    "Paladin" => ["Devotion", "Ancient"],
-    "Rogue" => ["Thief", "Assassin"]
+    "Barbarian" => $barbarian_data,
+    "Paladin" => $paladin_data,
+    "Rogue" => $rogue_data
 ];
 
 $races = ["Human", "Elf", "Dwarf", "Halfling"];
@@ -19,19 +30,15 @@ function modifier($score) {
     return floor(($score - 10) / 2);
 }
 
-// Randomly pick class & subclass
 $classKeys = array_keys($classes);
 $selectedClass = $classKeys[array_rand($classKeys)];
-$selectedSubclass = $classes[$selectedClass][array_rand($classes[$selectedClass])];
+$classData = $classes[$selectedClass];
 
-// Random level between 1 and 5
+$selectedSubclass = $classData["subclasses"][array_rand($classData["subclasses"])] ?? "";
 $level = rand(1, 5);
-
-// Random race and name
 $selectedRace = $races[array_rand($races)];
 $selectedName = $names[array_rand($names)];
 
-// Generate abilities
 $abilities = [
     "str" => random_ability_score(),
     "dex" => random_ability_score(),
@@ -42,76 +49,31 @@ $abilities = [
 ];
 
 $proficiency_bonus = ($level < 5) ? 2 : 3;
-
-// Basic score
 $basic_score = ($selectedClass === "Rogue") ? "dex" : "str";
 
-// Weapons by class
-$weaponsByClass = [
-    "Barbarian" => [
-        ["name" => "Greataxe", "die" => "d12"],
-        ["name" => "Handaxe", "die" => "d6"]
-    ],
-    "Paladin" => [
-        ["name" => "Longsword", "die" => "d8"],
-        ["name" => "Javelin", "die" => "d6"]
-    ],
-    "Rogue" => [
-        ["name" => "Shortsword", "die" => "d6"],
-        ["name" => "Dagger", "die" => "d4"]
-    ]
-];
-
-$selectedWeapons = $weaponsByClass[$selectedClass] ?? [];
-
-$armor = [
+$selectedWeapons = $classData["weapons"] ?? [];
+$armor = $classData["armor"] ?? [
     "name" => "Leather Armor",
     "description" => "AC = 11 + Dex modifier"
 ];
 
-$features_by_class = [
-    "Barbarian" => [
-        ["name" => "Rage", "description" => "Advantage on STR checks, +2 damage, resistance to physical dmg."],
-        ["name" => "Reckless Attack", "description" => "Advantage on STR melee attacks, enemies have adv on you."]
-    ],
-    "Paladin" => [
-        ["name" => "Divine Smite", "description" => "Extra radiant damage on a hit by expending spell slots."],
-        ["name" => "Lay on Hands", "description" => "Heal a target by using your pool of healing power."]
-    ],
-    "Rogue" => [
-        ["name" => "Sneak Attack", "description" => "Extra damage when you have advantage or ally near target."],
-        ["name" => "Cunning Action", "description" => "Bonus action to Dash, Disengage, or Hide."]
-    ]
-];
-
-$selectedFeatures = $features_by_class[$selectedClass] ?? [];
-
+$selectedFeatures = $classData["features"] ?? [];
 if ($selectedClass === "Barbarian" && $level >= 5) {
     $selectedFeatures[] = ["name" => "Extra Attack", "description" => "You can attack twice with Attack action."];
 }
 
-// Calculate derived stats
-$armor_class = 10 + modifier($abilities['dex']);  
+$armor_class = 10 + modifier($abilities['dex']);
 if ($armor['name'] === "Leather Armor") {
     $armor_class = 11 + modifier($abilities['dex']);
 }
 
 $initiative = modifier($abilities['dex']);
 $speed = ($selectedRace === "Dwarf" || $selectedRace === "Halfling") ? 25 : 30;
-if ($selectedClass === "Barbarian") {
-    $speed += 10;
-}
+$speed += $classData["speed_bonus"] ?? 0;
 
-// Hit points: Using class-based hit die
-$hit_die = [
-    "Barbarian" => 12,
-    "Paladin"   => 10,
-    "Rogue"     => 8
-];
-$hd = $hit_die[$selectedClass] ?? 8;
-$hp = $hd + modifier($abilities['con'])*(int)$level + ($hd*($level-1));
+$hit_die = $classData["hit_die"] ?? 8;
+$hp = $hit_die + modifier($abilities['con'])*(int)$level + ($hit_die*($level-1));
 
-// Final character array
 $character = [
     "name" => $selectedName,
     "class" => $selectedClass,
